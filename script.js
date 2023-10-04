1f39ac032024d1bef5ece012002a252b50d5b1de
paulvu2023 <118864214+paulvu2023@users.noreply.github.com>
2023-10-03 21:28:45 -0700

    Add prettyPrint function to print out BST's

--- script.js
+++ script.js
@@ -29,3 +29,16 @@ class Tree {
     return Array.from(new Set(array));
   }
 }
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
